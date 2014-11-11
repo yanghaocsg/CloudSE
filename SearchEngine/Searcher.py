b562ef22bab68b59ca26ce6072446507a23990cc
yanghaocsg <287546756@qq.com>
2014-11-11 15:43:46 +0800

    add SearchEngine/SearchServer.py

--- SearchEngine/Searcher.py
+++ SearchEngine/Searcher.py
@@ -28,19 +28,21 @@ class Searcher(object):
         self.collection = self.db[company]
         self.collection.ensure_index([('tag', 1)], unique=True,  background=True, dropDups =True)
         self.ofh_tag = open(Path(self.cwd, 'tag_%s.txt' % company), 'w+')
-    def process(self, query=''):
+    def process(self, query='', start=0, num=20):
         try:
             list_s = yhTrieSeg.seg(query)
             logger.error('query %s' % '|'.join(list_s))
-            if not list_s:
-                return {}
-            else:
-                list_res = []
+            list_res = []
+            if list_s:
+                list_res = self.parse_query(list_s, start, end)
                 for s in self.parse_query(list_s):
                     list_res.append((s, 'http://www.120ask.com/question/%s.html'%s))
-                return simplejson.dumps(list_res)
+            dict_res = {'seg':list_s, 'res':list_res, 'status':0}
+            return simplejson.dumps(dict_res)
         except:
+            dict_res={'status':2, 'errlog':traceback.format_exc()}
             logger.error(traceback.format_exc())
+            return simplejson.dumps(dict_res)
             
     def parse_query(self, list_s=[]):
         set_docid = set()
@@ -57,5 +59,20 @@ class Searcher(object):
         logger.error('list_docid %s' % list_docid[:20])
         return list_docid[:20]
         
+searcher = Searcher()
+class Search_Handler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    @tornado.gen.engine
+    def get(self):
+        try:
+            dict_qs = YhTool.yh_urlparse_params(self.request.uri, ['query', 's', 'n'], ['', '0', '20'])
+            query, start, num = dict_qs['query'], int(dict_qs['s']), int(dict_qs['n'])
+            return searcher.process(query, start, num)
+        except Exception:
+            logger.error('svs_handler error time[%s][%s][%s]'% (self.request.request_time(), traceback.format_exc(), self.request.uri))
+            self.write(simplejson.dumps({'status':1, 'errlog':traceback.format_exc(), 'url':self.request.uri}))
+        finally:
+            self.finish()
+            
 if __name__=='__main__':
     Searcher().process(u'糖尿病')
